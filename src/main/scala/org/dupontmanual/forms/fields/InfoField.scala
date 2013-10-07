45aeee050dfe897495e98e9a8c9173085ae1cea7
Todd O'Bryan <toddobryan@gmail.com>
2013-10-07 05:56:16 -0400

    fixed InfoField to not be required and to use initial data

--- src/main/scala/org/dupontmanual/forms/fields/InfoField.scala
+++ src/main/scala/org/dupontmanual/forms/fields/InfoField.scala
@@ -4,8 +4,12 @@ import org.dupontmanual.forms.widgets.Widget
 import scala.xml.{ Attribute, Null, Text }
 
 class InfoField(name: String, value: String) extends TextField(name) {
+  override def required = false
   override def initialVal = Some(value)
   override def widgetAttrs(widget: Widget) = {
     super.widgetAttrs(widget).append(Attribute("disabled", Text("disabled"), Null))
   }
+  // disabled fields aren't sent in POST requests, so need to get data from 
+  // initial values
+  override def boundData(data: Seq[String], initial: Seq[String]) = initial
 }
